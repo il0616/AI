003e557dcb5703bf827d1b7753942d13bb935de6
BaeHongil <il0616@knu.ac.kr>
2016-03-15 03:18:44 +0900

    HW2-1 complete

--- HW1.py
+++ HW1.py
@@ -6,21 +6,26 @@ max_count = 0
 max_line = 0
 words = {}
 for i, line in enumerate(lines, 1):
-    now_count = line.count(' ') + 1
-    total_count += now_count
-    if( now_count > max_count ):
-        max_count = now_count
-        max_line = i
-
     w = line.split()
     for word in w:
-        if not words in words:
+        if word not in words:
+            words[word] = 1
+        else:
+            words[word] += 1
 
-    words = {() for word in w }
+    now_count = len(w)
+    total_count += now_count
+    if( now_count > max_count ):
+         max_count = now_count
+         max_line = i
 
+max_word = 0
+for word in words:
+    if words[word] > words.get(max_word, 0):
+        max_word = word
 
 print("단어수 : %d" % total_count)
 print("줄수 : %d" % len(lines))
-print("가장 많이 쓰인 단어 : %d")
+print("가장 많이 쓰인 단어 : %s" % max_word)
 print("단어가 가장 많이 포함된 라인 : %d" % max_line)
 print("단어가 가장 많이 포함된 라인의 단어 수 : %d" % max_count)
